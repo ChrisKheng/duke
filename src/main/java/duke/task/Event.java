0620e3d9ac10074d19f1066ca5327e5a0afbf463
Chris Kheng <chriskheng1609@gmail.com>
2019-09-10 12:37:11 +0800

    Refactor code of Task class and its subclasses

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -5,6 +5,7 @@ package duke.task;
  */
 public class Event extends Task {
     private String date;
+    private String[] tentativeDates;
 
     /**
      * Constructs an Event object.
@@ -14,9 +15,34 @@ public class Event extends Task {
      */
     public Event(String topic, String date) {
         super(topic);
-        this.date = date;
         this.type = "E";
-        this.details = String.format("%s (at: %s)", topic, date);
-        this.detailsForDatabase = String.format("%s | %s", topic, date);
+        this.date = date;
+        // this.details = String.format("%s (at: %s)", topic, date);
+        // this.detailsForDatabase = String.format("%s | %s", topic, date);
+    }
+
+    public Event(String topic, String[] tentativeDates) {
+        this(topic, "hello");
+    }
+
+    /**
+     * Returns the string representation of the task.
+     * It takes the form of [type][done status][details].
+     *
+     * @return string representation of the task.
+     */
+    @Override
+    public String toString() {
+        return String.format("[%s][%s] %s (at: %s)", type, status, title, date);
+    }
+
+    /**
+     * Returns the data summary of this task to record this task in the database.
+     *
+     * @return the data summary of this task.
+     */
+    public String getSummaryForDatabase() {
+        int status = isDone ? 1 : 0;
+        return String.format("%s | %d | %s | %s", type, status, title, date);
     }
 }
--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -7,12 +7,32 @@ public class ToDo extends Task {
     /**
      * Constructs a ToDo object.
      *
-     * @param topic the topic of the ToDo.
+     * @param title the topic of the ToDo.
      */
-    public ToDo(String topic) {
-        super(topic);
+    public ToDo(String title) {
+        super(title);
         this.type = "T";
-        this.details = topic;
-        this.detailsForDatabase = topic;
+    }
+
+    /**
+     * Returns the string representation of the task.
+     * It takes the form of [type][done status][details].
+     *
+     * @return string representation of the task.
+     */
+    @Override
+    public String toString() {
+        return String.format("[%s][%s] %s", type, status, title);
+    }
+
+    /**
+     * Returns the data summary of this task to record this task in the database.
+     *
+     * @return the data summary of this task.
+     */
+
+    public String getSummaryForDatabase() {
+        int status = isDone ? 1 : 0;
+        return String.format("%s | %d | %s", type, status, title);
     }
 }
